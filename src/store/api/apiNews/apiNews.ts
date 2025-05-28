4a0cea35b8f34714b96aa20a18d954696ff42146
Michael Shonov <waveafterwave3w2@gmail.com>
2025-05-28 15:21:36 +0300

    first commit

--- src/store/api/apiNews/apiNews.ts
+++ src/store/api/apiNews/apiNews.ts
@@ -1,15 +1,14 @@
-import { NEWS_API_KEY, NEWS_BASE_URL } from '../../../data/data'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: NEWS_BASE_URL,
+        baseUrl: 'url',
     }),
     tagTypes: ['News'],
     endpoints: (builder) => ({
         getNews: builder.query({
-            query: () => `latest-news?apiKey=${NEWS_API_KEY}`,
+            query: () => `latest-news?apiKey=apikey`,
         }),
     }),
 })
