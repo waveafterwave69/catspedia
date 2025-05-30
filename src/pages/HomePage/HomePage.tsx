bbe1c6412412301029a1ab1766f88257a39bc8a6
Michael Shonov <waveafterwave3w2@gmail.com>
2025-05-30 17:40:02 +0300

    fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
 import Breeds from '../../components/Breeds/Breeds'
 import Promo from '../../components/Promo/Promo'
-import { useGetCatsQuery } from '../../store/api/apiNews/apiCats'
 
 const HomePage: React.FC = () => {
     return (
