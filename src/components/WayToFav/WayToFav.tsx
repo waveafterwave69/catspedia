16d3353b4b3ba075a9eb853c8d615e22737dbc89
Michael Shonov <waveafterwave3w2@gmail.com>
2025-06-01 23:02:35 +0300

    cat cat cat

--- src/components/WayToFav/WayToFav.tsx
+++ src/components/WayToFav/WayToFav.tsx
@@ -2,13 +2,14 @@ import styles from './WayToFav.module.css'
 
 import pawsAll from '../../img/pawsAll.png'
 import favImg from '../../img/favImg.png'
+import { Link } from 'react-router'
 
 const WayToFav: React.FC = () => {
     return (
         <>
             <section className={styles.fav}>
                 <h2 className={styles.fav__title}>
-                    possibility of including cats in your
+                    possibility of including cats in your{' '}
                     <span>favorites!</span>
                 </h2>
                 <div className={styles.fav__row}>
@@ -17,13 +18,15 @@ const WayToFav: React.FC = () => {
                             To add a cat to your favorites, just click on the
                             star next to its photo!
                         </p>
-                        <button className={styles.fav__button}>
-                            List of cats
-                        </button>
+                        <Link to="/list">
+                            <button className={styles.fav__button}>
+                                List of cats
+                            </button>
+                        </Link>
                     </div>
-                    <img src={favImg} alt="" className={styles.fav__image} />
+                    <img src={favImg} alt="" className={styles.fav__cat__img} />
                 </div>
-                <img src={pawsAll} alt="paws" />
+                <img src={pawsAll} alt="paws" className={styles.fav__image} />
             </section>
         </>
     )
