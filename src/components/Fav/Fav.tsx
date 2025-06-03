336847098483985d8a6d3649298a5a21e3105dcc
Michael Shonov <waveafterwave3w2@gmail.com>
2025-06-03 15:23:51 +0300

    complete!

--- src/components/Fav/Fav.tsx
+++ src/components/Fav/Fav.tsx
@@ -4,25 +4,21 @@ import CatBreedItem from '../CatBreedItem/CatBreedItem'
 
 const Fav: React.FC = () => {
     const favorites = useSelector((state: any) => state.fav.fav)
-    console.log(favorites)
-
     return (
         <section className={styles.cats}>
-            <>
-                <h2 className={styles.cats__title}>cat breeds</h2>
-                <ul className={styles.cats__list}>
-                    {favorites.map((cat: any) => (
-                        <li key={cat.id} className={styles.list__item}>
-                            <CatBreedItem
-                                newData={favorites}
-                                breedId={cat.id}
-                                name={cat.name}
-                                button={true}
-                            />
-                        </li>
-                    ))}
-                </ul>
-            </>
+            <h2 className={styles.cats__title}>favorites</h2>
+            <ul className={styles.cats__list}>
+                {favorites.map((cat: any) => (
+                    <li key={cat.id} className={styles.list__item}>
+                        <CatBreedItem
+                            newData={favorites}
+                            breedId={cat.id}
+                            name={cat.name}
+                            button={true}
+                        />
+                    </li>
+                ))}
+            </ul>
         </section>
     )
 }
