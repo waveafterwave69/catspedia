f986b6a182922752a1cc0cf9195034845eac3bb8
Michael Shonov <waveafterwave3w2@gmail.com>
2025-06-03 21:49:40 +0300

    complete 2.0!

--- src/pages/FavPage/FavPage.tsx
+++ src/pages/FavPage/FavPage.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react'
 import Fav from '../../components/Fav/Fav'
 
 const FavPage: React.FC = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
     return (
         <>
             <Fav />
