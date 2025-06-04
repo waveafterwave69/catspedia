9b7ee2ca3067af19fc7d004c757a87da7966ff12
Michael Shonov <waveafterwave3w2@gmail.com>
2025-06-04 13:27:28 +0300

    search

--- src/components/CatsList/CatsList.tsx
+++ src/components/CatsList/CatsList.tsx
@@ -1,16 +1,34 @@
+import React, { useState } from 'react'
 import { useGetCatsQuery } from '../../store/api/apiNews/apiCats'
 import CatBreedItem from '../CatBreedItem/CatBreedItem'
 import styles from './CatsList.module.css'
 
 const CatsList: React.FC = () => {
+    const [searchTerm, setSearchTerm] = useState('')
     let { data, isLoading } = useGetCatsQuery('')
+
+    const filteredCats = data?.filter((cat: any) =>
+        cat.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value)
+    }
+
     return (
         <section className={styles.cats}>
             {!isLoading && (
                 <>
                     <h2 className={styles.cats__title}>cat breeds</h2>
+                    <input
+                        type="text"
+                        placeholder="Search for a cat breed..."
+                        className={styles.cats__input}
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
                     <ul className={styles.cats__list}>
-                        {data.map((cat: any) => (
+                        {filteredCats.map((cat: any) => (
                             <li key={cat.id} className={styles.list__item}>
                                 <CatBreedItem
                                     newData={data}
